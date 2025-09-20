523c2c26f7f1f9d7516a926b2f9709cb0b059355
Eduardo <eduardobragadeoliveira@gmail.com>
2025-09-20 01:27:31 -0400

    mudado forma de visualização das relações do trigger ativado

--- scripts/3_triggerAtivado.sql
+++ scripts/3_triggerAtivado.sql
@@ -1,14 +1,47 @@
-/* Mostrando antes */
-SELECT * FROM pagamentos AS antes_pagamento;
-SELECT * FROM assinaturas_ativas AS antes_assinatura;
-SELECT * FROM usuarios AS antes_usuario;
+USE trabalho;
 
-/* Executando a operação: */
+SELECT 
+    u.id AS usuario_id,
+    u.nome AS nome_usuario,
+    u.email,
+    u.plano AS plano_usuario,
+    a.id AS assinatura_id,
+    a.plano AS plano_assinatura,
+    a.tempo_plano,
+    a.data_inicio,
+    a.data_fim,
+    a.status AS status_assinatura,
+    p.id AS pagamento_id,
+    p.valor_pago,
+    p.metodo_pagamento,
+    p.data_pagamento,
+    p.status AS status_pagamento
+FROM pagamentos p
+INNER JOIN assinaturas_ativas a ON p.assinatura_id = a.id
+INNER JOIN usuarios u ON a.usuario_id = u.id
+WHERE p.id = 8;
+
 UPDATE pagamentos
 SET status = 'pago'
-WHERE id = 1;
+WHERE id = 8;
 
-/* Mostrando o resultado */
-SELECT * FROM pagamentos AS depois_pagamento;
-SELECT * FROM assinaturas_ativas AS depois_assinatura;
-SELECT * FROM usuarios AS depois_usuario;
+SELECT 
+    u.id AS usuario_id,
+    u.nome AS nome_usuario,
+    u.email,
+    u.plano AS plano_usuario,
+    a.id AS assinatura_id,
+    a.plano AS plano_assinatura,
+    a.tempo_plano,
+    a.data_inicio,
+    a.data_fim,
+    a.status AS status_assinatura,
+    p.id AS pagamento_id,
+    p.valor_pago,
+    p.metodo_pagamento,
+    p.data_pagamento,
+    p.status AS status_pagamento
+FROM pagamentos p
+INNER JOIN assinaturas_ativas a ON p.assinatura_id = a.id
+INNER JOIN usuarios u ON a.usuario_id = u.id
+WHERE p.id = 8;
